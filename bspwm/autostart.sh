cd8b9aa1d7d10a5c1df29dc728bc9e753797b59d
Erik Dubois <erik.dubois@gmail.com>
2018-09-29 22:12:30 +0200

    merging two configs - spotify fix - full screen for youtub, vlc, netflix etc

--- bspwm/autostart.sh
+++ bspwm/autostart.sh
@@ -6,21 +6,32 @@ function run {
     $@&
   fi
 }
-#run "xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal"
-#run "xrandr --output LVDS1 --mode 1366x768 --output DP3 --mode 1920x1080 --right-of LVDS1"
-#run "xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off"
+
+#Find out your monitor name with xrandr or arandr (save and you get this line)
+#xrandr --output VGA-1 --primary --mode 1360x768 --pos 0x0 --rotate normal
+#xrandr --output DP2 --primary --mode 1920x1080 --rate 60.00 --output LVDS1 --off &
+#xrandr --output LVDS1 --mode 1366x768 --output DP3 --mode 1920x1080 --right-of LVDS1
+#xrandr --output HDMI2 --mode 1920x1080 --pos 1920x0 --rotate normal --output HDMI1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
 
 $HOME/.config/polybar/launch.sh &
+
+#change your keyboard if you need it
 #setxkbmap -layout be
+
+#Some ways to set your wallpaper besides variety or nitrogen
 feh --bg-scale ~/.config/bspwm/wall.png &
+#feh --randomize --bg-fill ~/Képek/*
+#feh --randomize --bg-fill ~/Dropbox/Apps/Desktoppr/*
+
 xsetroot -cursor_name left_ptr &
 sxhkd &
-run nm-applet &
+
+conky -c $HOME/.config/bspwm/system-overview &
 run variety &
+run nm-applet &
 run pamac-tray &
 run xfce4-power-manager &
 numlockx on &
-conky -c $HOME/.config/bspwm/system-overview &
 compton --config $HOME/.config/bspwm/compton.conf &
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 /usr/lib/xfce4/notifyd/xfce4-notifyd &
@@ -30,3 +41,4 @@ compton --config $HOME/.config/bspwm/compton.conf &
 #blueberry-tray &
 #thunar &
 #insync start &
+run discord &
