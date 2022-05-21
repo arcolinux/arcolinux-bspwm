00ee0323ed02b249e39a629975148f7f3a85bd14
arcolinuxz <arcolinuxinfo@gmail.com>
2022-05-21 07:38:52 +0200

    wallpaper

--- etc/skel/.config/bspwm/autostart.sh
+++ etc/skel/.config/bspwm/autostart.sh
@@ -30,7 +30,8 @@ fi
 #Some ways to set your wallpaper besides variety or nitrogen
 #feh --bg-scale ~/.config/bspwm/wall.png &
 feh --bg-fill /usr/share/backgrounds/arcolinux/arco-wallpaper.jpg &
-#feh --randomize --bg-fill ~/Képek/*
+#wallpaper for other Arch based systems
+#feh --bg-fill /usr/share/archlinux-tweak-tool/data/wallpaper/wallpaper.png &
 #feh --randomize --bg-fill ~/Dropbox/Apps/Desktoppr/*
 
 dex $HOME/.config/autostart/arcolinux-welcome-app.desktop
